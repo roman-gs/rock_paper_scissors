6ac85ec61c9f81a4263f5ff80b1cad5c651fbbb5
roman-gs <roman_gs@hotmail.fr>
2022-12-20 09:45:29 +0000

    Import random library and define user and computer variables

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -12,14 +13,21 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('rock_paper_scissors')
 
-def get_user_name():
-    """
-    """
-    data_str = input('Please enter your username: \n')
-    print(f"Welcome to Rock Paper Scissors, {data_str}")
-    worksheet = SHEET.add_worksheet(title=data_str, rows=100, cols=20)
+#def get_user_name():
+    #"""
+    #Get the username from the user and creates a worksheet for each new user
+    #"""
+    #data_str = input('Please enter your username: \n')
+    #print(f"Welcome to Rock Paper Scissors, {data_str}")
+    #worksheet = SHEET.add_worksheet(title=data_str, rows=100, cols=20)
 
-get_user_name()
+#get_user_name()
+
+user = input('Choose from Rock, Paper, or Scissors: ')
+computer = random.choice(["Rock", "Paper", "Scissors"])
+
+print(user)
+print(computer)
 
 
 
