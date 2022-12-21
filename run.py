0e019f2b5c2b63f0c27f230b19b2f95e65394a73
roman-gs <roman_gs@hotmail.fr>
2022-12-21 14:49:37 +0000

    UPPER CASE all global variables

--- run.py
+++ run.py
@@ -7,72 +7,88 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
-     
+
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('rock_paper_scissors')
 
 
-def get_user_name(): #Get the username from the user and creates a worksheet for each new user
-    global data_str
-    data_str = input('Please enter your username: \n')
+def get_user_name():
+    """
+    Get the username from the user and creates a worksheet for each new user
+    """
+    global DATA_STR
+    DATA_STR = input('Please enter your username: \n')
     try:
-        worksheet = SHEET.add_worksheet(title=data_str, rows=1, cols=2)
-    except:
-        print("This username is already taken, please pick a different one") # Ask the user to pick a different name if name already exits
+        worksheet = SHEET.add_worksheet(title=DATA_STR, rows=1, cols=2)
+    except NameError():
+        print("This username is already taken, please pick a different one")
         get_user_name()
 
-    print(f"Welcome to Rock Paper Scissors, {data_str}")
+    print(f"Welcome to Rock Paper Scissors, {DATA_STR}")
     score = [0 ,0] #Set the score to 0 - 0
-    SHEET.worksheet(data_str).append_row(score) 
-
-get_user_name()
+    SHEET.worksheet(DATA_STR).append_row(score) 
 
 
-def get_user_answer(): # Ask the user to make a choice
-    global user
+def get_user_answer(): 
+    """
+    Ask the user to make a choice and validates it
+    """
+    global USER
     choice = input('Please choose from rock, paper, or scissors: ')
     choice_strip = choice.strip()
-    user = choice_strip.lower()
-    if user == "rock" or user == "paper" or user == "scissors": # Validate the user choice
+    USER = choice_strip.lower()
+    if USER == "rock" or USER == "paper" or USER == "scissors":
         print()
-        print(f'You chose {user}')
+        print(f'You chose {USER}')
     else:
         print()
         print("Your input is incorrect...")
         get_user_answer()
-        
-def get_computer_answer(): # Generate random computer choice
-    global computer
-    computer = random.choice(["rock", "paper", "scissors"])
-    print(f'Your opponent chose {computer}')
 
-def play_game(user, computer): # Compare the user and the computer choices
-    if user == computer:
+
+def get_computer_answer():
+    """
+    Generate random computer choice
+    """
+    global COMPUTER
+    COMPUTER = random.choice(["rock", "paper", "scissors"])
+    print(f'Your opponent chose {COMPUTER}')
+
+
+def play_game(USER, COMPUTER): 
+    """
+    Compare the user and the computer choices
+    """
+    if USER == COMPUTER:
         print()
         print("It's a tie")
         print()
-        calculate_score(data_str)
+        calculate_score(DATA_STR)
         play_again()
 
-    if (user == 'rock' and computer == 'scissors') or (user == 'paper' and computer == 'rock') or (user == 'scissors' and computer == 'paper'):
+    if (USER == 'rock' and COMPUTER == 'scissors') or (USER == 'paper' and COMPUTER == 'rock') or (USER == 'scissors' and COMPUTER == 'paper'):
         print()
         print('You won')
         print()
-        increment_user_score(data_str)
-        calculate_score(data_str)
+        increment_user_score(DATA_STR)
+        calculate_score(DATA_STR)
         play_again()
 
     else:
         print()
         print('You lost')
         print()
-        increment_computer_score(data_str)
-        calculate_score(data_str)
+        increment_computer_score(DATA_STR)
+        calculate_score(DATA_STR)
         play_again()
 
-def play_again(): # Allow the user to exit the program or keep playing after each result
+
+def play_again():
+    """
+    Allow the user to exit the program or keep playing after each result
+    """
     answer = input("Do you want to keep playing? y or n ...\n")
     answer_strip = answer.strip()
     answer_lower = answer_strip.lower()
@@ -82,37 +98,55 @@ def play_again(): # Allow the user to exit the program or keep playing after eac
     if answer_lower == "y":
         get_user_answer()
         get_computer_answer()
-        play_game(user, computer)
+        play_game(USER, COMPUTER)
     
     else:
         print()
         print("Your input is incorrect, please choose Y for yes or N for no")
         play_again()
 
-def increment_user_score(data_str): # Increment the user score by 1
+
+def increment_user_score(DATA_STR): 
+    """
+    Increment the user score by 1 and the computer score by 0
+    """
     new_score = [1, 0]
-    update_score = SHEET.worksheet(data_str).append_row(new_score)
+    update_score = SHEET.worksheet(DATA_STR).append_row(new_score)
 
-def increment_computer_score(data_str): # Increment the computer score by 1
+
+def increment_computer_score(data_str): 
+    """
+    Increment the computer score by 1 and the user score by 0
+    """
     new_score = [0, 1]
-    update_score = SHEET.worksheet(data_str).append_row(new_score)
+    update_score = SHEET.worksheet(DATA_STR).append_row(new_score)
 
-def calculate_score(data_str): # Calculate the user and computer score
-    current_user_score = SHEET.worksheet(data_str).col_values(1) 
-    current_user_score_int = [eval(i) for i in current_user_score] # Convert str in the "current_user_score" list to int
+
+def calculate_score(DATA_STR):
+    """
+    Calculate the user and computer scores
+    """
+    current_user_score = SHEET.worksheet(DATA_STR).col_values(1)
+    #Convert str in the "current_user_score" list to int 
+    #Cred: https://www.geeksforgeeks.org/python-converting-all-strings-in-list-to-integers/
+    current_user_score_int = [eval(i) for i in current_user_score]
     user_score = sum(current_user_score_int)
 
-    current_computer_score = SHEET.worksheet(data_str).col_values(2)
-    current_computer_score_int = [eval(i) for i in current_computer_score] 
+    current_computer_score = SHEET.worksheet(DATA_STR).col_values(2)
+    current_computer_score_int = [eval(i) for i in current_computer_score]
     computer_score = sum(current_computer_score_int)
 
-    print(f'Score is: {data_str} {user_score}, computer {computer_score} \n')
-
-get_user_answer()
-get_computer_answer()
-play_game(user, computer)
+    print(f'Score is: {DATA_STR} {user_score}, computer {computer_score} \n')
 
 
-    
+def main():
+    """
+    Call all program functions
+    """
+    get_user_name()
+    get_user_answer()
+    get_computer_answer()
+    play_game(USER, COMPUTER)
 
 
+main()
